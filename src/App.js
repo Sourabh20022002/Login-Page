c497684e6edc130ddbf15ed1b5960e4289f7da3b
Sourabh Singh <Sourabh@iiitmanipur.ac.in>
2022-07-16 19:43:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Login from './components/Login'
+import Login3 from './components/Login3';
+import Login2 from './components/Login2'
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Login2 />
+      <Login3 />
+    </div>
+  );
+}
+
+export default App;
